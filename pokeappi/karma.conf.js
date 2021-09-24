0a55459ae872130001b3960f667c87b13a98444f
Michelle <michellelaurian@gmail.com>
2021-09-24 18:27:59 -0300

    create folders

--- pokeappi/karma.conf.js
+++ pokeappi/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/desafiofornt'),
+      dir: require('path').join(__dirname, './coverage/pokeappi'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- pokeappi/src/app/app.component.spec.ts
+++ pokeappi/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'desafiofornt'`, () => {
+  it(`should have as title 'pokeappi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('desafiofornt');
+    expect(app.title).toEqual('pokeappi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('desafiofornt app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pokeappi app is running!');
   });
 });
