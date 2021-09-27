3ed1808f427a85a7ca7bfff3aef8217b0e6372b2
Michelle <michellelaurian@gmail.com>
2021-09-27 11:43:53 -0300

    create search bar

--- pokeapi/src/app/shared/components/fom-search/fom-search.component.ts
+++ pokeapi/src/app/shared/components/fom-search/fom-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fom-search',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FomSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSearch(value: string){
+    console.log('Buscar=', value)
+
+      if(value && value.length > 3){
+        this.router.navigate(['/character-list'],
+        {queryParams: {q: value}}
+        )
+      }
   }
 
 }
